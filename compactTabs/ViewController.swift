4325c46250fb6a350310c996650ea4363172a1c2
Tay Kai Quan <kaitay303@gmail.com>
2022-08-25 11:40:05 +0800

    Initial Commit

---
+++ compactTabs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  compactTabs
+//
+//  Created by TAY KAI QUAN on 25/8/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
