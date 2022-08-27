e0f822e5d5c582f18beb6027351b9d7a0ca9c36e
Tay Kai Quan <kaitay303@gmail.com>
2022-08-27 20:06:47 +0800

    Open new window when dock item clicked and no windows open

--- compactTabs/AppDelegate.swift
+++ compactTabs/AppDelegate.swift
@@ -10,8 +10,16 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                if (window.delegate?.isKind(of: MainWindowController.self)) == true {
+                    window.makeKeyAndOrderFront(self)
+                }
+            }
+        }
+        return true
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
